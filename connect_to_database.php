1b176dbea8ef31f55fa488a0e7c1dc11b4c03eb6
seanmac4477 <43825293+seanmac4477@users.noreply.github.com>
2018-10-04 13:48:01 -0500

    Update connect_to_database.php

--- connect_to_database.php
+++ connect_to_database.php
@@ -18,7 +18,7 @@ try {
         <meta charset="utf-8">
         <title>PHP MySQL </title>
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="style.css" rel="stylesheet" type="text/css">
     </head>
 
     <body>
